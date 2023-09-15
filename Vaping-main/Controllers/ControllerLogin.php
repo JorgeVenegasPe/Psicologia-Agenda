87c50abe2c1f51c2103ee158cad25017e6c83287
programadordc <137285862+programadordc@users.noreply.github.com>
2023-09-15 13:33:31 -0500

    Daniel Actualizacion_15_09_23
    Mejora del DashBoard y en el formato responsive.

--- Vaping-main/Controllers/ControllerLogin.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    if (isset($_POST["usu"]) && isset($_POST["pass"])) {
-        require("../Models/ModelLogin.php");
-        $validar = new Login();
-        $validar->validarDatos($_POST["usu"], $_POST["pass"]);
-    } else {
-        header("location: ../Views/Login.php");
-    }
-?>
--- Vaping-main/Crud/EliminarProducto.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once("C:/xampp/htdocs/Vaping-main/Controllers/ControllerProducto.php");
-$obj = new usernameControlerProducto();
-$obj->EliminarProducto($_GET['id']);
-?>
